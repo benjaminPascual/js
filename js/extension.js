61cbccb8e3ca241148f4b100cd15d63cd0be2655
benjaminPascual <benjaminpascual199@gmail.com>
2022-06-06 19:19:35 -0300

    aplicando funciones

--- js/extension.js
+++ js/extension.js
@@ -1,19 +1,30 @@
+function pedir(mensaje) {
+	return prompt(mensaje);
+}
+function pedirNumero(mensaje) {
+	return parseInt(mensaje);
+}
+function multiplicadorNeto() {
+	let porcentaje = pedirNumero(pedir("ingrese el porcentaje de ganancia que desea"));
+		return ganancia = 1 + (porcentaje/100)
+}
+
 let precio = 0
 let ganancia = 0
-let inicio = prompt( "desea calcular el precio de un producto? (si/no)")
+let inicio = pedir("desea calcular el precio de un producto? (si/no)");
+
 while(inicio == "si"){
-	let cantidad = parseInt( prompt("ingrese la candidad de productos a calcular"))
-	let porcentaje = parseInt( prompt("ingrese el porcentaje de ganancia que desea"))
-		ganancia = 1 + (porcentaje/100)
+	let cantidad = pedirNumero(pedir("ingrese la candidad de productos a calcular"));
+	multiplicadorNeto();
 
 	for (i = 0; i < cantidad; i++) {
 
-		let producto = prompt("ingrese el nombre del producto")
-		let costo = parseInt( prompt("ingrese el costo del producto"))
+		let producto = pedir("ingrese el nombre del producto")
+		let costo = pedirNumero(pedir("ingrese el costo del producto"));
 			precio = costo*ganancia
-		console.log("el precio de "+producto+" es "+precio+" (" +porcentaje+" %)")
+		console.log("el precio de "+producto+" es "+precio)
 		
 	}
 
-	inicio = prompt("se te olvido algun producto? desea agregar mas? (si/no)")
+	inicio = pedir("se te olvido algun producto? desea agregar mas? (si/no)");
 }
