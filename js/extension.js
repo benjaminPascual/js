2a060a74224df095d4700767a40f25e63ccc0b9c
benjaminPascual <benjaminpascual199@gmail.com>
2022-06-08 11:36:48 -0300

    correcciones + agregado de consicion estricta si/no

--- js/extension.js
+++ js/extension.js
@@ -1,30 +1,37 @@
-function pedir(mensaje) {
+function pedirDato(mensaje) {
 	return prompt(mensaje);
 }
 function pedirNumero(mensaje) {
-	return parseInt(mensaje);
+	return parseInt(prompt(mensaje));
 }
 function multiplicadorNeto() {
-	let porcentaje = pedirNumero(pedir("ingrese el porcentaje de ganancia que desea"));
+	let porcentaje = pedirNumero("ingrese el porcentaje de ganancia que desea");
 		return ganancia = 1 + (porcentaje/100)
 }
 
 let precio = 0
 let ganancia = 0
-let inicio = pedir("desea calcular el precio de un producto? (si/no)");
+let inicio = pedirDato("desea calcular el precio de un producto? (si/no)");
 
-while(inicio == "si"){
-	let cantidad = pedirNumero(pedir("ingrese la candidad de productos a calcular"));
-	multiplicadorNeto();
+do{
+	if (inicio ==="si" || inicio==="no") {
+		while(inicio == "si"){
+			let cantidad = pedirNumero("ingrese la candidad de productos a calcular");
+			multiplicadorNeto();
 
-	for (i = 0; i < cantidad; i++) {
+			for (i = 0; i < cantidad; i++) {
 
-		let producto = pedir("ingrese el nombre del producto")
-		let costo = pedirNumero(pedir("ingrese el costo del producto"));
-			precio = costo*ganancia
-		console.log("el precio de "+producto+" es "+precio)
-		
+				let producto = pedirDato("ingrese el nombre del producto")
+				let costo = pedirNumero("ingrese el costo del producto");
+					precio = costo*ganancia
+				console.log("el precio de "+producto+" es "+precio)
+				
+			}
+
+			inicio = pedirDato("se te olvido algun producto? desea agregar mas? (si/no)");
+		}
+	} else{
+		alert("su respuesta debe ser si o no")
+		inicio = pedirDato("desea calcular el precio de un producto? (si/no)");
 	}
-
-	inicio = pedir("se te olvido algun producto? desea agregar mas? (si/no)");
-}
+}while(inicio ==="si")
